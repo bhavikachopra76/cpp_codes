99422ff650e57fcf4d5e2f2a48f48ea26547d0ed
bhavikachopra06 <bhavikachopra67@gmail.com>
2026-04-24 20:22:07 +0530

    LeetCode Questions 🚀

---
+++ leetcode/leetcode_1207.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool uniqueOccurrences(vector<int>& arr) {
+        //1 2 2 1 1 3
+    vector<int> a;
+    int count = 0;
+    for (int i=0; i<arr.size(); i++)
+    {
+        
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter size of vector : ";
+    cin >> n;
+
+    vector<char>s;
+
+    for (int i=0; i<n; i++)
+    {
+        char x;
+        cin>>x;
+        s.push_back(x);
+    }
+
+
+    return 0;
+}
